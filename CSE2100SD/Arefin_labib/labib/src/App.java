9a9642e0c7f125f8882c3c16027dd6d93e010d3e
piru72 <parvezdirom2000@gmail.com>
2021-12-19 23:21:17 +0600

    arefin_labibs_problem_solved

---
+++ CSE2100SD/Arefin_labib/labib/src/App.java
@@ -0,0 +1,56 @@
+
+import java.lang.Math;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        double a = 56.34;
+        double b = 6.58334;
+        double c = 34.4264;
+
+        System.out.println("Value of Pi     " + Math.PI);
+        System.out.println("Value of E      " + Math.E);
+
+        System.out.println("Value of random number  " + Math.random());
+
+        System.out.println("Absolute value of C     " + Math.abs(c));
+
+        System.out.println("Square root of a        " + Math.sqrt(a));
+
+        System.out.println("Maximum value between a and b " + Math.max(a, b));
+
+        System.out.println("Calculate the value a^b       " + Math.pow(a, b));
+
+        System.out.println("Round the number a      " + Math.round(a));
+
+        System.out.println("Calculate the value of sqrt(a^2 + b^2) " + Math.hypot(a, b));
+
+        System.out.println("Floor value of b    " + Math.floor(b));
+
+        System.out.println("Ceil value of b     " + Math.ceil(b));
+
+        System.out.println("Round value of b    " + Math.round(b));
+
+
+        System.out.println("Floor value of c    " + Math.floor(c));
+
+        System.out.println("Ceil value of c     " + Math.ceil(c));
+
+        System.out.println("Round value of c    " + Math.round(c));
+
+
+        System.out.println("Radian value of a   " + Math.toRadians(a));
+
+        System.out.println("Find the sin value of a" + Math.sin(a));
+
+        
+
+
+
+
+
+
+
+    }
+}
