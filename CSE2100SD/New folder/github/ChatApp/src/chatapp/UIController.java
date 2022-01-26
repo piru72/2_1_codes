ba50d25c224f0b092ad08e0be4ee508d87568751
piru72 <parvezdirom2000@gmail.com>
2022-01-27 05:18:07 +0600

    Ui of chat app added

---
+++ CSE2100SD/New folder/github/ChatApp/src/chatapp/UIController.java
@@ -0,0 +1,21 @@
+package chatapp;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class UIController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
