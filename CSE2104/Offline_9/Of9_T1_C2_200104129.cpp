b7bd7f7173e060e86337e4af715b590fff13a3b3
piru72 <parvezdirom2000@gmail.com>
2022-03-01 21:57:16 +0600

    Oflline 9 done

--- CSE2104/Offline_9/Of9_T1_C2_200104129.cpp
+++ CSE2104/Offline_9/Of9_T1_C2_200104129.cpp
@@ -19,6 +19,30 @@ struct Node
 
 Node *root = NULL;
 
+struct Tuples_2
+{
+    Node *p;
+
+    Tuples_2(Node *n)
+    {
+        p = n;
+    }
+
+    int getPVal()
+    {
+        return p->data;
+    }
+    Node *left()
+    {
+        return p->left;
+    }
+    Node *right()
+    {
+        return p->right;
+    }
+
+};
+
 struct Tuples
 {
     Node *p;
@@ -116,14 +140,114 @@ void pre_order(Node *node)
 
 void in_order(Node *node)
 {
+
+    stack<Tuples> st;
+    Tuples t = Tuples(node, 1);
+    st.push(t);
+
+    while (!st.empty())
+    {
+        t = st.top();
+        st.pop();
+
+        if (t.p != NULL)
+        {
+            switch (t.i)
+            {
+            case 1:
+            {
+                t.setI(2);
+                st.push(t);
+                Tuples leftT = Tuples(t.left(), 1);
+                st.push(leftT);
+                break;
+            }
+            case 2:
+            {
+
+                t.setI(3);
+                st.push(t);
+                cout << t.getPVal() << " ";
+                break;
+            }
+            case 3:
+            {
+                Tuples rightT = Tuples(t.right(), 1);
+                st.push(rightT);
+                break;
+            }
+            }
+        }
+    }
 }
 
 void post_order(Node *node)
 {
+
+    stack<Tuples> st;
+    Tuples t = Tuples(node, 1);
+    st.push(t);
+
+    while (!st.empty())
+    {
+        t = st.top();
+        st.pop();
+
+        if (t.p != NULL)
+        {
+            switch (t.i)
+            {
+            case 1:
+            {
+
+                t.setI(2);
+                st.push(t);
+                Tuples leftT = Tuples(t.left(), 1);
+                st.push(leftT);
+                break;
+            }
+            case 2:
+            {
+                t.setI(3);
+                st.push(t);
+                Tuples rightT = Tuples(t.right(), 1);
+                st.push(rightT);
+                break;
+            }
+            case 3:
+            {
+
+                cout << t.getPVal() << " ";
+                break;
+            }
+            }
+        }
+    }
 }
 
 void level_order(Node *node)
 {
+    queue<Tuples_2> Q;
+
+    Tuples_2 t = Tuples_2(node);
+    Q.push(t);
+
+    while (!Q.empty())
+    {
+        t = Q.front();
+        Q.pop();
+        if (t.p != NULL)
+        {
+            cout << t.getPVal() << " ";
+           
+
+            Tuples_2 leftT = Tuples_2(t.left());
+            Q.push(leftT);
+
+            Tuples_2 rightT = Tuples_2(t.right());
+            Q.push(rightT);
+        }
+    }
 }
 
 int main()
@@ -135,14 +259,35 @@ int main()
     cout << endl;
 
     cout << "In order:" << endl;
-    //in_order(root);
+    in_order(root);
     cout << endl;
 
     cout << "Post order:" << endl;
-    //post_order(root);
+    post_order(root);
     cout << endl;
 
     cout << "Level order:" << endl;
-    //level_order(root);
+    level_order(root);
     cout << endl;
 }
+
+/*
+Pre order:
+2 7 1 6 5 10 9 8 3 4
+In order:
+1 7 5 6 10 2 9 3 8 4
+Post order:
+1 5 10 6 7 3 4 8 9 2
+Level order:
+2 7 9 1 6 8 5 10 3 4
+*/
+
+  /*          2
+          /       \
+         7         9
+        / \         \
+       1   6         8
+          / \       / \
+          5  10     3   4
+
+  */
